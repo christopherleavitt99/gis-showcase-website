f36a1fbb08ddff7718a7cc28a5442f5917263bdc
christopherleavitt99 <cml95@byui.edu>
2024-12-04 20:42:07 -0700

    Changed code to match project

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+ import "./index.css"
 
-function App() {
-  const [count, setCount] = useState(0)
+
+export default function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+    <div className="Title">
+      <h1>Chris Leavitt GIS Showcase</h1>
+    </div>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <a href="https://vite.dev" target="_blank">
+          <img src={"./images/byuibuildings.png"} className="logo" alt="Vite logo" />
+        </a>
+        </div>
+      <div className="card">
+        <a href="https://react.dev" target="_blank">
+          <img src={"./images/califire.png"} className="logo react" alt="React logo" />
+        </a>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
-
-export default App
