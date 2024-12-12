484bb36b25edd01d3c8759614e2c66af25f23ae1
christopherleavitt99 <cml95@byui.edu>
2024-12-12 13:04:14 -0700

    minor fix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
-import module from 'next-images'
+// import module from 'next-images'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-module.exports = {
-  images: {
-    localPatterns: [
-      {
-        pathname: './images/**',
-        search: '',
-      },
-    ],
-  },
-};
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
+// module.exports = {
+//   images: {
+//     localPatterns: [
+//       {
+//         pathname: './images/**',
+//         search: '',
+//       },
+//     ],
+//   },
+// };
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
  import "./index.css"
-import "./next.config.js"
 
 export default function App() {
 
