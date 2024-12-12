37b768d7446da50a449e4d8ff606559728fb01df
christopherleavitt99 <cml95@byui.edu>
2024-12-12 12:42:58 -0700

    Updated images

---
+++ next.config.js
@@ -0,0 +1,12 @@
+import module from 'next-images'
+
+module.exports = {
+  images: {
+    localPatterns: [
+      {
+        pathname: './images/**',
+        search: '',
+      },
+    ],
+  },
+};
