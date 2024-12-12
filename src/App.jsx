dd5384818afd73dbbdc4cb5c3de28671814457a3
christopherleavitt99 <cml95@byui.edu>
2024-12-12 12:46:29 -0700

    added back brackets

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ export default function App() {
         </div>
       <div className="card" >
         <a href="https://storymaps.arcgis.com/stories/066b6dc5e9964d14aac45158ffd79a2b" >
-          <img src="./images/califire.png" />
+          <img src={"califire.png"} />
           <h2>California Wildfire Zones</h2>
           </a>
       </div>
