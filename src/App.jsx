6c1ed4258dda602d5d97ace9ea24cb7b8cb0a2ff
christopherleavitt99 <cml95@byui.edu>
2024-12-11 19:31:32 -0700

    image test

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ export default function App() {
       </div>
       <div className="card" >
         <a href="https://byui.maps.arcgis.com/apps/dashboards/4f9f7a6f3f73476aa90c086044cfe698" >
-          <img src={"./images/sporiquad.png"} />
+          <img src={"/images/sporiquad.png"} />
         <h2>Spori Quad</h2>
         </a>
       </div>
