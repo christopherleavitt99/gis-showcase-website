3663b8108ce697e018f591fba11843acb5f9f231
christopherleavitt99 <cml95@byui.edu>
2024-12-12 13:35:27 -0700

    minor change

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,7 @@ export default function App() {
       </div>
       <div className="card" >
         <a href="https://byui.maps.arcgis.com/apps/mapviewer/index.html?webmap=f7305df881cc453e962bbb25a0b988d9" >
-          <img src={"./images/hail.png"} />
+          <image src={"./images/hail.png"} />
         <h2>Hail Damage in corn fields</h2>
         </a>
       </div>
